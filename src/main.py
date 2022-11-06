f8bde39e67d8aa95cc42bcf9875753201d439a70
dubbl <dubbel14@googlemail.com>
2022-11-06 01:09:22 +0100

    add more structure to novel output

--- src/main.py
+++ src/main.py
@@ -1,17 +1,58 @@
 import argparse
 
+from pydantic import BaseModel
 from pydriller.repository import Repository
+
+
+class Chapter(BaseModel):
+    title: str = ''
+    paragraphs: list[str] = []
+
+    def print(self) -> None:
+        print(self.title)
+        for paragraph in self.paragraphs:
+            print(paragraph)
+
+
+class Novel(BaseModel):
+    title: str = ''
+    author: str = 'Novelopment 0.1'
+    synopsis: str = ''
+    chapters: list[Chapter] = []
+
+    def new_chapter(self, **kwargs) -> Chapter:
+        chapter = Chapter(**kwargs)
+        self.chapters.append(chapter)
+        return chapter
+
+    def print(self) -> None:
+        print(self.title)
+        print(f'By {self.author}\n')
+        print(self.synopsis)
+        for chapter in self.chapters:
+            chapter.print()
 
 
 def main():
     args = parser.parse_args()
     repo = load_repo(args.repository)
-    novel = ''
+    novel = Novel(title=f'The story of {get_repo_name(args.repository)}')
+
+    chapter_all = novel.new_chapter(title='All commits')
     for commit in repo.traverse_commits():
         first_line_commit_msg = commit.msg.split('\n')[0]
-        novel += f'{commit.author.name} be like {first_line_commit_msg}\n'
-    print(novel)
-    print(len(novel.split(' ')))
+        chapter_all.paragraphs.append(
+            f'{commit.author.name} be like {first_line_commit_msg}',
+        )
+
+    novel.print()
+
+
+def get_repo_name(path):
+    if path.endswith('.git'):
+        return path.split('/')[-1][:-4]
+    # TODO: handle local repos
+    return ''
 
 
 def load_repo(filepath):
