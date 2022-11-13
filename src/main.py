190a907900f0ad7800e92a9f7d9cdecb2c10c82d
dubbl <dubbel14@googlemail.com>
2022-11-13 16:36:11 +0100

    add and use black code formatter

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from pydriller.repository import Repository
 
 
 class Chapter(BaseModel):
-    title: str = ''
+    title: str = ""
     paragraphs: list[str] = []
 
     def print(self) -> None:
@@ -18,9 +18,9 @@ class Chapter(BaseModel):
 
 
 class Novel(BaseModel):
-    title: str = ''
-    author: str = 'Novelopment 0.1'
-    synopsis: str = ''
+    title: str = ""
+    author: str = "Novelopment 0.1"
+    synopsis: str = ""
     chapters: list[Chapter] = []
 
     def new_chapter(self, **kwargs) -> Chapter:
@@ -30,7 +30,7 @@ class Novel(BaseModel):
 
     def print(self) -> None:
         print(self.title)
-        print(f'By {self.author}\n')
+        print(f"By {self.author}\n")
         print(self.synopsis)
         for chapter in self.chapters:
             chapter.print()
@@ -43,24 +43,24 @@ def main():
     if not seed:
         seed = list(repo.traverse_commits())[-1].hash
     random.seed(int(seed, 16))
-    title_adj = random.choice(pycorpora.words.adjs['adjs'])
+    title_adj = random.choice(pycorpora.words.adjs["adjs"])
     novel = Novel(
-        title=f'The {title_adj} story of {get_repo_name(args.repository)}',
+        title=f"The {title_adj} story of {get_repo_name(args.repository)}",
     )
 
-    chapter_all = novel.new_chapter(title='All commits')
+    chapter_all = novel.new_chapter(title="All commits")
     for commit in repo.traverse_commits():
-        first_line_commit_msg = commit.msg.split('\n')[0]
+        first_line_commit_msg = commit.msg.split("\n")[0]
         chapter_all.paragraphs.append(
-            f'{commit.author.name} be like {first_line_commit_msg}',
+            f"{commit.author.name} be like {first_line_commit_msg}",
         )
 
     novel.print()
 
 
 def get_repo_name(repository_location):
-    if repository_location.endswith('.git'):
-        return repository_location.split('/')[-1][:-4]
+    if repository_location.endswith(".git"):
+        return repository_location.split("/")[-1][:-4]
     return Path(repository_location).absolute().stem
 
 
@@ -69,26 +69,26 @@ def load_repo(filepath):
 
 
 parser = argparse.ArgumentParser(
-    prog = 'novelopment',
-    description = 'Generate the story of a git repository.',
-    epilog = 'Commit for adventure!',
+    prog="novelopment",
+    description="Generate the story of a git repository.",
+    epilog="Commit for adventure!",
 )
 parser.add_argument(
-    'repository',
-    help='Filepath or URL of the git repository',
+    "repository",
+    help="Filepath or URL of the git repository",
 )
 parser.add_argument(
-    '-s',
-    '--seed',
-    action='store',
-    dest='seed',
+    "-s",
+    "--seed",
+    action="store",
+    dest="seed",
     help=(
-        'Hexadecimal seed of the pseudorandom number generator. '
-        'Defaults to the hash of the latest commit.'
+        "Hexadecimal seed of the pseudorandom number generator. "
+        "Defaults to the hash of the latest commit."
     ),
     required=False,
 )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
